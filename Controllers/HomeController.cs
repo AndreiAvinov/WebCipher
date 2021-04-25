755a741cd4f7dd17f7a9020ec56488955bb22707
AndreiAvinov <andreiivanchihin.q@ya.ru>
2021-04-25 09:57:34 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCipher.Models;
+using System.IO;
+using System.Text;
+
+namespace WebCipher.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (TempData["cipheredText"] != null && (string)TempData["cipheredData"] != "")
+            {
+                var form = TempDataHelper.Get<CipherModel>(TempData, "cipherForm");
+                form.OutputText = (string)TempData["cipheredText"];
+                return View(form);
+            }
+            return View(new CipherModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Decipher([Bind("InputText,InputFile,Direction,Key,UseFile,FileEncoding")] CipherModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                string cipheredText;
+                if (form.UseFile)
+                {
+                    cipheredText = CipherModel.DoCiphering(form.InputFile, form.Key, form.Direction == "Cipher", form.FileEncoding);
+                }
+                else
+                {
+                    cipheredText = CipherModel.DoCiphering(form.InputText, form.Key, form.Direction == "Cipher");
+                }
+                TempData["cipheredText"] = cipheredText;
+                TempDataHelper.Put(TempData, "cipherForm", form);
+                return RedirectToAction(nameof(Index));
+            }
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFileDOCX([Bind("OutputText")] CipherModel form)
+        {
+            var content = form.OutputText;
+            var contentType = "application/octet-stream";
+            var fileName = "result.docx";
+            XWPFDocument doc = new();
+            var par = doc.CreateParagraph();
+            var run = par.CreateRun();
+            run.SetText(content);
+            using MemoryStream stream = new();
+            doc.Write(stream);
+            byte[] byteContent = stream.ToArray();
+            return File(byteContent, contentType, fileName);
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFileTXT([Bind("OutputText")] CipherModel form)
+        {
+            byte[] content = new UTF8Encoding(true).GetBytes(form.OutputText);
+            var contentType = "application/octet-stream";
+            var fileName = "result.txt";
+            return File(content, contentType, fileName);
+        }
+    }
+}
---
+++ Models/CipherModel.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WebCipher.Models
+{
+    public class CipherModel
+    {
+        [JsonIgnore]
+        public IFormFile InputFile { get; set; }
+        public string FileEncoding { get; set; } = "auto";
+        public string Direction { get; set; } = "Cipher";
+        public string InputText { get; set; } = "";
+        public string Key { get; set; } = "";
+        public bool UseFile { get; set; } = false;
+        private static string[] permittedExtensions = { ".txt", ".docx" };
+        public string OutputText { get; set; }
+        public static string DoCiphering(string text, string key, bool isForward)
+        {
+            if (String.IsNullOrEmpty(text))
+                return "";
+            if (String.IsNullOrEmpty(key))
+                return text;
+            List<int> numberfiedWord = new();
+            string ans = "";
+            int index = 0;
+            char[] alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            key = key.ToLower();
+            foreach (var item in key)
+            {
+                if (alph.Contains(item))
+                {
+                    numberfiedWord.Add(Array.IndexOf(alph, item));
+                }
+                else if (Char.IsDigit(item))
+                {
+                    numberfiedWord.Add(item - '0');
+                }
+                else
+                {
+                    numberfiedWord.Add(0);
+                }
+            }
+            text = text.ToLower();
+            foreach (char item in text)
+            {
+                if (alph.Contains(item))
+                {
+                    int itemIndex = Array.IndexOf(alph, item);
+                    ans += alph[(33 + itemIndex + (isForward ? 1 : -1) * numberfiedWord[index % numberfiedWord.Count]) % alph.Length];
+                    index++;
+                }
+                else
+                {
+                    ans += item;
+                }
+            }
+            return ans;
+        }
+
+        public static string DoCiphering(IFormFile textFile, string key, bool isForward, string fileEncoding)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            if (textFile == null)
+            {
+                return "Ошибка: файл не загружен";
+            }
+            var ext = Path.GetExtension(textFile.FileName).ToLowerInvariant();
+            if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+            {
+                return "Ошибка: поддерживаются только форматы txt и docx";
+            }
+            if (ext == ".txt")
+            {
+                using (Stream stream = textFile.OpenReadStream())
+                {
+                    if (fileEncoding == "auto")
+                    {
+                        using (StreamReader sr = new(stream, true))
+                        {
+                            return DoCiphering(sr.ReadToEnd(), key, isForward);
+                        }
+                    }
+                    else if (fileEncoding == "ansi")
+                    {
+                        using (StreamReader sr = new(stream, Encoding.GetEncoding(1251), true))
+                        {
+                            return DoCiphering(sr.ReadToEnd(), key, isForward);
+                        }
+                    }
+                }
+            }
+            if (ext == ".docx")
+            {
+                using (Stream stream = textFile.OpenReadStream())
+                {
+                    string docxText = "";
+                    XWPFDocument doc = new(stream);
+                    foreach (var item in doc.Paragraphs)
+                    {
+                        docxText += item.Text;
+                    }
+                    return DoCiphering(docxText, key, isForward);
+                }
+            }
+            return "";
+        }
+}
+}
